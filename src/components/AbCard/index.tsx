7885db44ca1e43c6f9bb9381011951959762b12e
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-01-31 15:43:25 -0300

    feat: add Card Component

--- src/components/AbCard/index.tsx
+++ src/components/AbCard/index.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const StyledCard = styled.div`
+const StyledCard = styled.div<CardProps>`
   padding: 48px;
   background: #ffffff;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
+
+  ${(props: CardProps) => {
+    if (props.size === 'sm') {
+      return css`
+         {
+          max-width: 380px;
+        }
+      `;
+    }
+    if (props.size === 'md') {
+      return css`
+         {
+          max-width: 550px;
+        }
+      `;
+    }
+    return css`
+       {
+        max-width: 725px;
+      }
+    `;
+  }}
 `;
 
 export interface CardProps {
-  children: React.ReactNode;
+  children?: React.ReactNode;
+  size?: 'sm' | 'md' | 'lg';
 }
 
-export const AbCard = ({ children }: CardProps) => {
-  return <StyledCard>{children}</StyledCard>;
+export const AbCard = ({ children, size = 'sm' }: CardProps) => {
+  return <StyledCard size={size}>{children}</StyledCard>;
 };
